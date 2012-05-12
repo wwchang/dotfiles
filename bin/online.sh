ddbbf09a8b0032a04020a29d903fb947be1491b5
stardiviner <numbchild@gmail.com>
2012-05-12 16:26:18 +0800

    update bin/online.sh

--- bin/online.sh
+++ bin/online.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO : find out what method is the best and safe way to detect online(connected).
-# ping is slow, find a mush faster way to check.
+# way1: ping is slow, find a mush faster way to check.
+# way2: use ifconfig.
 
-if ( ping -q -c 3 baidu.com && return 1 ); then
-    echo "connect network"
-    return 0
+if [[ $(ping -q -c 5 baidu.com && exit 1) ]]; then
+    #echo "connect network"
+    exit 0
 else
-    return 1
+    #echo "failed"
+    exit 1
 fi
