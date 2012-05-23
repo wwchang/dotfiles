7f815e2ccb3c3dbfbd01730a7b32d780308bd681
stardiviner <numbchild@gmail.com>
2012-05-23 22:09:23 +0800

    remove trvialing spaces

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -4,20 +4,20 @@
 # gain root at first
 ROOT_UID=0
 if [ ${UID} -ne ${ROOT_UID} ]
-then 
+then
     echo "please execute this script with root privilt "
     exit
-fi 
+fi
 
-# list out which disk you installed linux system 
-df -l 
-# let you select disk 
-read -p "Enter your linux disk: (like sda1)  " disk 
+# list out which disk you installed linux system
+df -l
+# let you select disk
+read -p "Enter your linux disk: (like sda1)  " disk
 
 mkdir /mnt/temp
-mount /dev/${disk} /mnt/temp 
-mount --bind /dev /mnt/temp/proc 
+mount /dev/${disk} /mnt/temp
+mount --bind /dev /mnt/temp/proc
 mount -t proc none /mnt/temp/proc
-mount --bind /sys /mnt/temp/sys 
-mount /mnt/temp /bin/bash 
-source /tec/profile 
+mount --bind /sys /mnt/temp/sys
+mount /mnt/temp /bin/bash
+source /tec/profile
