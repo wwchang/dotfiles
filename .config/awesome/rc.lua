eef10df0097edf0fe363d907b248199a8fc407a6
stardiviner <numbchild@gmail.com>
2012-06-19 11:37:36 +0800

    add a function to redirect commands or scripts output to widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -98,6 +98,18 @@ require("bashets")
         end
     end
     -- }}}
+
+    -- {{{ executing commands and scripts and output to widget
+    function execute_command(command)
+        local fh = io.popen(command)
+        local str = ""
+        for i in fh:lines() do
+            str = str .. i
+        end
+        io.close(fh)
+        return str
+    end
+    -- }}}
 -- }}}
 
 -- {{{ [ Theme ]
