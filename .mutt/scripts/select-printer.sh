8ead45c819cd0870ec884e3b708296ca142e210f
stardiviner <numbchild@gmail.com>
2012-07-17 17:01:53 +0800

    use more smart shebang for mutt scripts.

--- .mutt/scripts/select-printer.sh
+++ .mutt/scripts/select-printer.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 printer=$(dialog --stdout --title "Printing $*" --menu "Select a printer:" 0 0 0 $(lpstat -v |awk '{print $3,$4}' |sed -r 's-^(.*?): (.*)$-\1 \2-'))
 [ ! -z $printer ] && lpr -P $printer $*
