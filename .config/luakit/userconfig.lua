df55de1647ff547c38e6bfbec94b75344a6800ea
stardiviner <numbchild@gmail.com>
2012-07-23 07:48:20 +0800

    add comment to declare append user custom modules to global settings

---
+++ .config/luakit/userconfig.lua
@@ -0,0 +1,9 @@
+------------------------------
+-- load user custom modules --
+------------------------------
+-- those custom module names are corresponding to global module names.
+-- Just append my settings from custom modules to global settings.
+
+-----------------------------
+-- end user custom modules --
+-----------------------------
