3686dc8484833c29eb21927f2f97fba59661d472
stardiviner <numbchild@gmail.com>
2012-07-27 07:56:57 +0800

    setup custom domain_props to set default font size and disable scripts/plugins, and set user stylesheet file

--- .config/luakit/globals_user.lua
+++ .config/luakit/globals_user.lua
@@ -7,3 +7,30 @@ search_engines.baidu = "http://www.baidu.com/s?wd=%s"
 search_engines.github = "http://github.com/search?q=%s"
 search_engines.wiki = "http://en.wikipedia.org/wiki/Special:Search?search=%s"
 search_engines.googlecode = "http://code.google.com/query/#q=%s"
+
+-- domain rules
+-- properties reference :
+-- http://webkitgtk.org/reference/webkitgtk/stable/WebKitWebSettings.html
+domain_props = {
+    ["all"] = {
+        -- font
+        sans_serif_font_family = "dejavu sans mono",
+        serif_font_family = "dejavu serif",
+        monospace_font_family = "monaco",
+        default_font_size = 16,
+        default_monospace_font_size = 16,
+        minimum_font_size = 13,
+        -- script & plugin
+        enable_scripts = false,
+        enable_plugins = false,
+        -- style
+        user_stylesheet_uri = nil,
+    },
+    ["github.com"] = {
+        user_stylesheet_uri = "file://" .. luakit.data_dir .. "/styles/github.css",
+    },
+    ["youtube.com"] = {
+        enable_scripts = true,
+        enable_plugins = true,
+    }
+}
