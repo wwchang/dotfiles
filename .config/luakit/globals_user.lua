855d484f0a4a125d2c0f7bd8d1d12519d55996fc
stardiviner <numbchild@gmail.com>
2012-07-27 07:55:48 +0800

    add my custome search engines for luakit

---
+++ .config/luakit/globals_user.lua
@@ -0,0 +1,9 @@
+-- append settings to global tables.
+-- e.g.  search_engines.google = "http://google.com/search?q=%s"
+
+-- [ search_engines ]
+search_engines.blekko = "http://blekko.com/ws/+%s"
+search_engines.baidu = "http://www.baidu.com/s?wd=%s"
+search_engines.github = "http://github.com/search?q=%s"
+search_engines.wiki = "http://en.wikipedia.org/wiki/Special:Search?search=%s"
+search_engines.googlecode = "http://code.google.com/query/#q=%s"
