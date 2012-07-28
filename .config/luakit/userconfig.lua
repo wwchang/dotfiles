642f969a6947437d1848f9ad2b388de042475a47
stardiviner <numbchild@gmail.com>
2012-07-28 20:56:29 +0800

    add globals_user module in user config file of luakit

--- .config/luakit/userconfig.lua
+++ .config/luakit/userconfig.lua
@@ -4,6 +4,7 @@
 -- those custom module names are corresponding to global module names.
 -- Just append my settings from custom modules to global settings.
 
+require "globals_user"
 require "webview_user"
 require "binds_user"
 require "window_user"
