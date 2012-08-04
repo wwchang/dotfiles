22c2bdee60a0d86d5a04e082a7097867827167b3
stardiviner <numbchild@gmail.com>
2012-08-04 19:06:17 +0800

    going to add github.py as Canto script for GitHub feed

---
+++ .canto/scripts/github.py
@@ -0,0 +1,13 @@
+# Canto GitHub script
+# by stardiviner [numbchild Gmail]
+# v1.0
+#
+# If put this in the plugins directory, it can improve github feed output:
+# Features:
+#   1. format output
+#   2. whole/complete context output.
+#   3. show commit diff.
+#   4. filter some items like: somebody start watching somebody.
+
+
+# TODO reference other github service integerent
