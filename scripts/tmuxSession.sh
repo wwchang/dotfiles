6fa89964c80d4fda181c7d97247decc587279f88
NagatoPain <numbchild@gmail.com>
2012-08-26 18:34:12 +0800

    update tmux session script

--- scripts/tmuxSession.sh
+++ scripts/tmuxSession.sh
@@ -32,11 +32,10 @@ tmux has-session -t $session1
 if [ $? != 0 ]; then
 
     # [ 1 command line prompt ]
-    tmux new-session -d -s $session1
-    # new-session -d -s Daily "elinks http://www.phrack.org/issues.html"
+    tmux new-session -d -s $session1 -n proxy 'python ~/censorship/GoAgent/goagent-1.0/goagent/local/proxy.py'
     tmux split-window -t $session1 -h
     # split-window -h 'cat ~/Git/dotfiles/sudo.pass | sudo -t nethogs -d 3 ppp0'
-    tmux select-pane -t $session1:1.1
+    tmux select-pane -t $session1:1.2
     # select-layout main-horizontal
 
     # [ 2 IRC & bitlbee ]
@@ -44,25 +43,25 @@ if [ $? != 0 ]; then
     tmux set-window-option -t $session1:2 monitor-activity on
 
     # [ 3 GTD ]
-    tmux new-window -t $session1 -n tasks \
-        'vim "-c set nospell" ~/vimwiki/wiki/plan.wiki'
+    tmux new-window -t $session1 -n note \
+        'vim "-c set nospell" ~/Wiki/vimwiki/wiki/plan.wiki'
     tmux split-window -t $session1:3.1 -h -p 40 \
-        'vim "-c set nospell" ~/vimwiki/wiki/Dreams/Dreams.wiki'
+        'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Dreams/Dreams.wiki'
     tmux split-window -t $session1:3.2 -v -p 60 \
-        'vim "-c set nospell" ~/vimwiki/wiki/Ideas/Ideas.wiki'
+        'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Ideas/Ideas.wiki'
     tmux split-window -t $session1:3.1 -v -p 30 \
-        'vim "-c set nospell" ~/vimwiki/wiki/Business/Business.wiki'
+        'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Me/Experience/Experience.wiki'
     tmux select-pane -t $session1:3.1
     #split-window -h -p 35 'task shell' # taskwarrior shell
 
     # [ 4 mail & news ]
     tmux new-window -t $session1 -n msg mutt
-    tmux split-window -t $session1:4.1 -h -p 30 'canto'
+    tmux split-window -t $session1:4.1 -h -p 30 'export http_proxy=127.0.0.1:8087 ; canto-fetch ; canto'
     tmux select-pane -t $session1:4.1
     tmux set-window-option -t $session1:4 monitor-activity on
 
     # [ 5 books ]
-    tmux new-window -t $session1 -n book \
+    tmux new-window -t $session1 -n data \
         'ranger /media/backup/Chris/Data/Computer/'
     tmux split-window -t $session1:5.1 -h \
         'ranger /media/backup/Chris/Data/'
@@ -90,23 +89,25 @@ fi
 tmux has-session -t $session2
 if [ $? != 0 ]; then
 
-    # [ vimwiki ]
-    # [ Python ]
-    tmux new-session -d -s $session2 -n python \
-        'vim ~/vimwiki/wiki/program/Python/Python.wiki'
+    # [ Wiki/vimwiki ]
+    # [ Linux ]
+    tmux new-session -d -s $session2 -n linux \
+        'vim ~/Wiki/vimwiki/wiki/Linux/Linux.wiki'
     # [ Program ]
     tmux new-window -t $session2 -n program \
-        'vim ~/vimwiki/wiki/program/program.wiki'
+        'vim ~/Wiki/vimwiki/wiki/program/program.wiki'
     # [ Languages ]
     tmux new-window -t $session2 -n lang \
-        'vim ~/vimwiki/wiki/program/Languages.wiki'
-    # [ Linux ]
-    tmux new-window -t $session2 -n linux \
-        'vim ~/vimwiki/wiki/Linux/Linux.wiki'
+        'vim ~/Wiki/vimwiki/wiki/program/Languages.wiki'
+    tmux new-window -t $session2 -n Ruby \
+        'vim ~/Wiki/vimwiki/wiki/program/Ruby/Ruby.wiki'
+    tmux new-window -t $session2 -n Python \
+        'vim ~/Wiki/vimwiki/wiki/program/Python/Python.wiki'
     # [ others ]
     tmux new-window -t $session2 -n misc
+    # new-session -d -s Daily "elinks http://www.phrack.org/issues.html"
 
-    tmux select-pane -t $session2:1.1
+    tmux select-pane -t $session2:3.2
 
     echo "Session $session2 has been created."
 else
