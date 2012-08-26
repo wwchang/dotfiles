7bc2cb9de60f2ec0a9c716933cccb6c7da7d2837
NagatoPain <numbchild@gmail.com>
2012-08-26 16:51:07 +0800

    remove useless bin/ scripts

--- bin/word.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Author: chris M. Sprite
-#Mail: (numbchild@gmail.com)
-#Notice: you can not use my file without my permit !
-#Description: add color for command "look"
-
-grep --color "$1" /usr/share/dict/words
