891cb6be537ff473185d6c48a111cf055f1f3dbc
NagatoPain <numbchild@gmail.com>
2012-09-05 09:18:39 +0800

    update tmuxsession script

--- scripts/tmuxSession.sh
+++ scripts/tmuxSession.sh
@@ -32,51 +32,53 @@ tmux has-session -t $session1
 if [ $? != 0 ]; then
 
     # [ 1 command line prompt ]
-    tmux new-session -d -s $session1 -n proxy 'python ~/censorship/GoAgent/goagent-1.0/goagent/local/proxy.py'
-    tmux split-window -t $session1 -h
+    tmux new-session -d -s $session1 -n network 'python ~/censorship/GoAgent/goagent-1.0/goagent/local/proxy.py'
+    tmux split-window -t $session1 -h 'bwm-ng'
+    # tmux split-window -t $session1 -h 'bmon'
+    # tmux split-window -t $session1 -h 'ifstat -w -z -S'
     # split-window -h 'cat ~/Git/dotfiles/sudo.pass | sudo -t nethogs -d 3 ppp0'
     tmux select-pane -t $session1:1.2
     # select-layout main-horizontal
 
-    # [ 2 IRC & bitlbee ]
-    tmux new-window -t $session1 -n irc 'online.sh && weechat-curses'
-    tmux set-window-option -t $session1:2 monitor-activity on
-
-    # [ 3 GTD ]
+    # [ 2 GTD ]
     tmux new-window -t $session1 -n note \
         'vim "-c set nospell" ~/Wiki/vimwiki/wiki/plan.wiki'
-    tmux split-window -t $session1:3.1 -h -p 40 \
+    tmux split-window -t $session1:2.1 -h -p 40 \
         'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Dreams/Dreams.wiki'
-    tmux split-window -t $session1:3.2 -v -p 60 \
+    tmux split-window -t $session1:2.2 -v -p 60 \
         'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Ideas/Ideas.wiki'
-    tmux split-window -t $session1:3.1 -v -p 30 \
+    tmux split-window -t $session1:2.1 -v -p 30 \
         'vim "-c set nospell" ~/Wiki/vimwiki/wiki/Me/Experience/Experience.wiki'
-    tmux select-pane -t $session1:3.1
+    tmux select-pane -t $session1:2.1
     #split-window -h -p 35 'task shell' # taskwarrior shell
 
-    # [ 4 mail & news ]
+    # [ 3 mail & news ]
     tmux new-window -t $session1 -n msg mutt
-    tmux split-window -t $session1:4.1 -h -p 30 'export http_proxy=127.0.0.1:8087 ; canto-fetch ; canto'
-    tmux select-pane -t $session1:4.1
-    tmux set-window-option -t $session1:4 monitor-activity on
+    tmux split-window -t $session1:3.1 -h -p 30 'export http_proxy=127.0.0.1:8087 ; canto-fetch ; canto'
+    tmux select-pane -t $session1:3.1
+    tmux set-window-option -t $session1:3 monitor-activity on
 
-    # [ 5 books ]
+    # [ 4 books ]
     tmux new-window -t $session1 -n data \
         'ranger /media/backup/Chris/Data/Computer/'
-    tmux split-window -t $session1:5.1 -h \
+    tmux split-window -t $session1:4.1 -h \
         'ranger /media/backup/Chris/Data/'
-    tmux split-window -t $session1:5.2 -v -p 40 \
+    tmux split-window -t $session1:4.2 -v -p 40 \
         'ranger ~/Downloads/'
-    tmux split-window -t $session1:5.1 -v -p 40 \
-        'ranger /media/backup/Work/'
-    tmux select-pane -t $session1:5.1
+    tmux split-window -t $session1:4.1 -v -p 40 \
+        'ranger /media/rest/MLdonkey/mlnet_incoming/files/'
+    tmux select-pane -t $session1:4.1
+
+    # [ 5 IRC & bitlbee ]
+    # tmux new-window -t $session1 -n irc 'online.sh && weechat-curses'
+    # tmux set-window-option -t $session1:5 monitor-activity on
 
     # [ 6 music ]
     # tmux new-window -t $session1 -n music ncmpcpp
     # tmux split-window -t $session1:6.1 -h alsamixer
     # tmux select-pane -t $session1:6.1
 
-    tmux select-pane -t $session1:3.1
+    tmux select-pane -t $session1:2.1
 
     echo "Session $session1 has been created."
 else
@@ -103,11 +105,17 @@ if [ $? != 0 ]; then
         'vim ~/Wiki/vimwiki/wiki/program/Ruby/Ruby.wiki'
     tmux new-window -t $session2 -n Python \
         'vim ~/Wiki/vimwiki/wiki/program/Python/Python.wiki'
+    tmux new-window -t $session2 -n JavaScript \
+        'vim ~/Wiki/vimwiki/wiki/program/JavaScript/JavaScript.wiki'
+    tmux new-window -t $session2 -n Go \
+        'vim ~/Wiki/vimwiki/wiki/program/Go/Go.wiki'
+    tmux new-window -t $session2 -n C \
+        'vim ~/Wiki/vimwiki/wiki/program/C/C.wiki'
     # [ others ]
-    tmux new-window -t $session2 -n misc
+    # tmux new-window -t $session2 -n misc
     # new-session -d -s Daily "elinks http://www.phrack.org/issues.html"
 
-    tmux select-pane -t $session2:3.2
+    tmux select-pane -t $session2:8.1
 
     echo "Session $session2 has been created."
 else
